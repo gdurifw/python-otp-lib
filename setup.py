a68dc83b4e57afd5e44dd595da0e5188a5dbd5d2
gdurifw <44707120+gdurifw@users.noreply.github.com>
2019-12-12 10:51:55 +0100

    remove fwb reference
    Hi marco, can you remove the fwb reference.
    Ciao

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
         version="0.1",
         packages=find_packages(),
         author="Marco Caimi",
-        author_email="marco.caimi@fastweb.it",
+        author_email="Marco Caimi",
         description="A simple library that performs HOTP and TOTP token generation.",
         license="GPL v3",
-        url="http://mi-ber-vlgit01.dev.fastcloud.fwb/mcaimi/python-otp-lib.git"
+        url="http://yuorserver/mcaimi/python-otp-lib.git"
 )
